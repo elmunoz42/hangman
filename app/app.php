f8c2de9d59562b557fab2a9a623950acc7a7def4
Carlos Munoz Kampff and Philip Putnam <philip@>
2017-02-09 11:56:52 -0800

    add initial files.

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Game.php';
+
+    $app = new Silex\Application();
+
+    $app->get('/', function() {
+
+        return "hello world";
+    });
+
+
+    return $app;
+?>
